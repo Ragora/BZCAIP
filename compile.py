3ef1198041a6634c9b685e7e01370f8ab124cf04
Robert MacGregor <ragoradev@gmail.com>
2015-03-16 15:23:22 -0400

    Forgot license ref on compile.py

--- compile.py
+++ compile.py
@@ -2,8 +2,8 @@
 	Compiler script that sticks the compiled version of BZCAIP in
 	the dist folder with the data.
 
-	This software is licensed under the Draconic Free License
-	version 1. Please refer to LICENSE.txt for more information.
+	This software is licensed under the MIT license. Refer to LICENSE.txt
+    for more information.
 """
 
 import shutil
